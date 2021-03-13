685fd5119013164587e84c3c25eb22ebad92a321
su-cimmiv <vincenzo.cimmino3@studenti.uniparthenope.it>
2021-03-13 19:57:56 +0100

    (+) Update Removed debug print

--- parsl/dataflow/dflow.py
+++ parsl/dataflow/dflow.py
@@ -668,7 +668,7 @@ class DataFlowKernel(object):
         
         for idx, f in enumerate(inputs):
             (inputs[idx], func) = self.data_manager.optionally_stage_in(f, func, executor)
-            print(inputs[idx])
+            
 
         for kwarg, f in kwargs.items():
             (kwargs[kwarg], func) = self.data_manager.optionally_stage_in(f, func, executor)
@@ -904,7 +904,7 @@ class DataFlowKernel(object):
                 workflow_app_name = app_kwargs.get('workflow_app_name',"")
             if workflow_app_name == "":
                 workflow_app_name = func.__name__+'-'+str(task_id)
-            print("workflow_app_name = ",workflow_app_name)
+            
             
             task_def.update({
                 'workflow_app_name':workflow_app_name,
