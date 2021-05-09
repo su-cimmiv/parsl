ab9192943ca43256a5b3d92b0cd71c8025c05b34
su-cimmiv <vincenzo.cimmino3@studenti.uniparthenope.it>
2021-05-09 16:04:53 +0200

    (+)Update: bug fix

--- parsl/app/Sandbox.py
+++ parsl/app/Sandbox.py
@@ -101,15 +101,14 @@ class Sandbox(object):
         """
 
         tasks = json.loads(self.tasks_dep)
-        print(tasks)
         stager = SandboxStager()
         info = script.split(" ")
         script = script.replace("workflow://", "")
         command = ""
         for i in range(len(info)):
             if 'workflow://' in info[i]:
-                dep_app_index = self.find_task_by_name(dep_app_info[1], tasks)
                 dep_app_info = info[i].replace("workflow://", "").split("/")
+                dep_app_index = self.find_task_by_name(dep_app_info[1], tasks)
                 dep_app_wd = tasks[dep_app_index]['working_directory']
                 dep_app_wf_name = dep_app_info[0]
                 src = dep_app_wf_name + "/" + dep_app_wd if dep_app_wf_name != "" else dep_app_wd
